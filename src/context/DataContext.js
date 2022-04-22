f937d77ae53db944d83c46ce36167bbf135c5ffd
maricoIR <matin.kahrizi@gmail.com>
2022-04-22 13:56:18 +0430

    Creating Context

---
+++ src/context/DataContext.js
@@ -0,0 +1,29 @@
+import React, { useReducer, createContext } from "react";
+
+const initialState = {
+  searchTerm: "",
+  editData: [],
+  data: [],
+  ids: [],
+  edit: false,
+  add: false,
+  reload: 1,
+};
+
+function reducer(state, action) {
+  return { ...state, ...action };
+}
+
+const DataContext = createContext();
+
+const DataProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <DataContext.Provider value={{ state, dispatch }}>
+      {children}
+    </DataContext.Provider>
+  );
+};
+
+export { DataContext, DataProvider };
