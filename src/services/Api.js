37497121249be51b825c9960b381fabf492b66bd
maricoIR <matin.kahrizi@gmail.com>
2022-04-22 13:56:49 +0430

    Config Node Server APIs

---
+++ src/services/Api.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+// axios.defaults.headers.common["Accept"] = `application/json`;
+
+axios.defaults.baseURL = "http://localhost:3001";
+
+export const getProducts = () => {
+  return axios.get("/products");
+};
+
+export const postProducts = (data) => {
+  return axios.post("/products/insert", {
+    title: data.title,
+    location: data.location,
+    buy: data.buy,
+    sell: data.sell,
+    count: data.count,
+  });
+};
+export const deleteProducts = (id) => {
+  return axios.delete("/products", { data: { id } });
+};
+
+export const putProduct = (id, data) => {
+  return axios.put("/products/" + id, {
+    title: data.title,
+    location: data.location,
+    buy: data.buy,
+    sell: data.sell,
+    count: data.count,
+  });
+};
