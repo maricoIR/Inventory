1668559392e91bf33bbac90bceaf739af14a7032
maricoIR <matin.kahrizi@gmail.com>
2022-04-22 13:57:47 +0430

    Home Componnent

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,5 +1,43 @@
+import Actionbar from "./components/Actionbar";
+import Table from "./components/Table";
+import Container from "./components/Container";
+import { useEffect } from "react";
+import Edit from "./components/Edit";
+import Add from "./components/Add";
+import { useContext } from "react";
+import { DataContext } from "../../context/DataContext";
+import "./styles.css";
+
 const Home = () => {
-  return <span className="text-[20px] text-rose-500">Just For Test</span>;
+  const {
+    state: { add, edit, searchTerm },
+    dispatch,
+  } = useContext(DataContext);
+
+  useEffect(() => {
+    dispatch({ searchTerm: "" });
+  }, [edit]);
+
+  if (edit) {
+    return (
+      <Container>
+        <Edit />
+      </Container>
+    );
+  }
+  if (add) {
+    return (
+      <Container>
+        <Add />
+      </Container>
+    );
+  }
+  return (
+    <Container>
+      <Actionbar />
+      <Table />
+    </Container>
+  );
 };
 
 export default Home;
